8169c056d9ed201cdb121147ef5ed612dffd4c46
josfam <josephamukun@tutanota.com>
2024-01-03 03:08:52 +0300

    Fix: Don't print `q`

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(97, 123):
-    if (num == 131 or num == 101):
-        pass
+    if (num == 113) or (num == 101):
+        continue
     else:
         print('{}'.format(chr(num)), end="")
