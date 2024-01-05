0e30451b763d401da071ad0d6bc73b9281ee28ed
josfam <josephamukun@tutanota.com>
2024-01-05 17:33:27 +0300

    Fix: Remove variable assigments

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for ch in str:
-        ascii_num = ord(ch)
         # shift by 32 to print the upper-cases
-        if (97 <= ascii_num <= 122):
-            print('{}'.format(chr(ascii_num - 32)), end='')
+        if (97 <= ord(ch) <= 122):
+            print('{}'.format(chr(ord(ch) - 32)), end='')
         else:
             print('{}'.format(ch), end='')
     print()
