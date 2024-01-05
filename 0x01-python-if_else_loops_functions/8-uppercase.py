eb734988986ee023a96031ce99997f5c138066e8
josfam <josephamukun@tutanota.com>
2024-01-05 17:30:50 +0300

    Fix: Use 2 prints with format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,8 @@ def uppercase(str):
     for ch in str:
         ascii_num = ord(ch)
         # shift by 32 to print the upper-cases
-        new_ch = chr(ascii_num - 32) if (97 <= ascii_num <= 122) else ch
-        print('{}'.format(new_ch), end='')
+        if (97 <= ascii_num <= 122):
+            print('{}'.format(chr(ascii_num - 32)), end='')
+        else:
+            print('{}'.format(ch), end='')
     print()
