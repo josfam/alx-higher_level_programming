98dab925db39502f46164d5f55aa0426641d173f
josfam <josephamukun@tutanota.com>
2024-01-09 11:54:24 +0300

    Fix: Start max at the first number, not 0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    max = 0
+    max = my_list[0]  # start with the first number
+
     for num in my_list:
         if (num > max):
             max = num
