6d8b666ca8b2e5d39519d998078153b36072653d
josfam <josephamukun@tutanota.com>
2024-01-09 10:52:39 +0300

    Implement `7. Tuples addition`

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a_left, a_right = None, None
+    b_left, b_right = None, None
+
+    if not (len(tuple_a)):
+        a_left, a_right = 0, 0
+    elif (len(tuple_a) == 1):
+        a_left, a_right = tuple_a[0], 0
+    else:
+        a_left, a_right = tuple_a[0], tuple_a[1]
+    
+    if not (len(tuple_b)):
+        b_left, b_right = 0, 0
+    elif (len(tuple_b) == 1):
+        b_left, b_right = tuple_b[0], 0
+    else:
+        b_left, b_right = tuple_b[0], tuple_b[1]
+    
+    return (a_left + b_left, a_right + b_right)
