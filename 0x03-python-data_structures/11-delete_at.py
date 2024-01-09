c71c1a72a4d80efb1f94c716b119ad197951c4ad
josfam <josephamukun@tutanota.com>
2024-01-09 18:12:03 +0300

    Fix: Point `my_list` to the new list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,4 +6,5 @@ def delete_at(my_list=[], idx=0):
     for i, num in enumerate(my_list):
         if not (i == idx):
             new_list.append(num)
-    return new_list
+    my_list = new_list
+    return my_list
