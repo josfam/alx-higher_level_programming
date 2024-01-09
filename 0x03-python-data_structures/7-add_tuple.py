64c063ceb6d5c20258874c36a8ac3752374764b5
josfam <josephamukun@tutanota.com>
2024-01-09 10:53:32 +0300

    Formatting: Fix whitespace

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,12 +9,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
         a_left, a_right = tuple_a[0], 0
     else:
         a_left, a_right = tuple_a[0], tuple_a[1]
-    
+
     if not (len(tuple_b)):
         b_left, b_right = 0, 0
     elif (len(tuple_b) == 1):
         b_left, b_right = tuple_b[0], 0
     else:
         b_left, b_right = tuple_b[0], tuple_b[1]
-    
+
     return (a_left + b_left, a_right + b_right)
