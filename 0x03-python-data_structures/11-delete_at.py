7dda05fdde225d4ae96d85ca0cc8772dbcd959de
josfam <josephamukun@tutanota.com>
2024-01-09 18:35:43 +0300

    Fix: Don't make a new list in the function

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,9 +2,5 @@
 def delete_at(my_list=[], idx=0):
     if idx >= len(my_list) or idx < 0:
         return my_list
-    new_list = []
-    for i, num in enumerate(my_list):
-        if not (i == idx):
-            new_list.append(num)
-    my_list = new_list
+    del my_list[idx]
     return my_list
