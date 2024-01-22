400d9ab1cf9a5d08067d490d7b2482ee6d47b01e
josfam <josephamukun@tutanota.com>
2024-01-22 12:30:58 +0300

    Fix: Use new variable for keeping count

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    count = 0
     for i in range(x):
         try:
             print(my_list[i], end='')
-            my_list[i+1]  # try to access the next element
+            count += 1
         except IndexError:
             break
     print()
-    return i + 1
+    return count
