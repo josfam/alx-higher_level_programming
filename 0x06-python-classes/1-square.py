0b8ed1605558b41c1eaa5108ab02e90e53fbd8e2
josfam <josephamukun@tutanota.com>
2024-01-25 00:07:43 +0300

    Implement `1. Square with size`

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""My first square with private instance variable"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        self.__size = size
