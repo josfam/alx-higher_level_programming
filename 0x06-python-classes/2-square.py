e2a2b16ede85f1f3267c0412ac6898e959d10a80
josfam <josephamukun@tutanota.com>
2024-01-25 08:18:12 +0300

    Implement `2. Size validation`

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""My first square with private instance variable"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        if not (isinstance(size, int)):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
