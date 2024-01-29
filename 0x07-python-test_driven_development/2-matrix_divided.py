2e95b60993cba52c50f53874637aea0ce45e38aa
josfam <josephamukun@tutanota.com>
2024-01-30 01:09:06 +0300

    Implement `0. Simple rectangle`

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Divides all elements in a matrix by a given divisor"""
+
+def matrix_divided(matrix, div):
+    """Divides all elements in an integer/float matrix by the given
+    divisor.
+    """
+    ...
---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Simulates a simple rectangle"""
+
+
+class Rectangle:
+    """Represents a simple rectangle"""
+
+    pass
