ce6d7a5233b0356c8c1476272e47894fe6af6301
josfam <josephamukun@tutanota.com>
2024-01-30 02:19:31 +0300

    Fix: Handle with or height == 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,4 +44,6 @@ class Rectangle:
 
     def perimeter(self):
         """Returns the perimeter of the rectangle"""
+        if self.width == 0 or self.height == 0:
+            return 0
         return 2 * (self.width + self.height)
