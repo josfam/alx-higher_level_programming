9945f2c86ede2a5357bd616672edcd4f235e79aa
josfam <josephamukun@tutanota.com>
2024-02-05 16:35:26 +0300

    Implement `1. My list`

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""1. My list
+Write a class MyList that inherits from list:
+
+Public instance method: def print_sorted(self): that prints the list, but sorted (ascending sort)
+You can assume that all the elements of the list will be of type int
+You are not allowed to import any module
+"""
+
+
+class MyList(list):
+    """A subclass of list, that can print a sorted list"""
+
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        """Prints the list, but in sorted order (from smallest to biggest)"""
+        list_copy = super().copy()
+        print(sorted(list_copy))
