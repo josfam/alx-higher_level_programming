5fceb4740deab135bfe57f7c51b8fd0cd5c88355
josfam <josephamukun@tutanota.com>
2024-02-05 21:54:36 +0300

    Implement `3. Same class or inherit from`

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Write a function that returns True if the object is an instance of, or if the
+object is an instance of a class that inherited from, the specified class ;
+otherwise False.
+
+Prototype: def is_kind_of_class(obj, a_class):
+You are not allowed to import any module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True if the object is an instance of, or if the object is an
+    instance of a class that inherited from, the specified class.
+    Returns False otherwise.
+    """
+    return isinstance(obj, a_class)
