267c1e0acbfb3ee2d589f4b7ebf04db27f15b590
josfam <josephamukun@tutanota.com>
2024-02-07 04:34:40 +0300

    Implement `10. Student to JSON with filter`

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Write a class Student that defines a student"""
+
+
+class Student:
+    """Defines a Student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        new_dict = dict()
+        if attrs and isinstance(attrs, list):
+            for key in attrs:
+                value = self.__dict__.get(key)
+                if not value:  # skip a non-existent key
+                    continue
+                new_dict.update({key: value})
+            return new_dict
+        else:
+            return self.__dict__
