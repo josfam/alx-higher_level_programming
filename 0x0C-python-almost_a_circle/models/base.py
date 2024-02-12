7a4fcd51aaaf38d7a816297d613628276fe99684
josfam <josephamukun@tutanota.com>
2024-02-12 19:50:07 +0300

    Implement `1. Base class`

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Write the first class Base:
+"""
+
+
+class Base:
+    """Represents the "base" of all other classes in the project"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
