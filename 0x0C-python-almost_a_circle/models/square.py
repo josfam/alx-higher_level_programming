9435eadd02afa2fc1115dcfe47a31751ea0a0eb5
josfam <josephamukun@tutanota.com>
2024-02-16 17:38:20 +0300

    Implement `11. Square size`

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,6 +11,17 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Returns the size of this square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of this square"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Human-readable representation of the Square"""
         id = self.id
