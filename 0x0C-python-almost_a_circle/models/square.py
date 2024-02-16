17d067b1449f7090bcb2f924da83262a18e4ee39
josfam <josephamukun@tutanota.com>
2024-02-16 16:16:38 +0300

    Fix: Call super using positional arguments only

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     """Represents a square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(width=size, height=size, x=x, y=y)
+        super().__init__(size, size, x, y)
 
     def __str__(self):
         """Human-readable representation of the Square"""
