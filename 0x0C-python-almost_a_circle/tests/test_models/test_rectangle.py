c1911affd536833430f9f229be8d9d15da985eac
josfam <josephamukun@tutanota.com>
2024-02-19 10:59:31 +0300

    Add setIp to rectangle tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,8 +1,16 @@
+#!/usr/bin/python3
+
 import unittest
 
+from models.base import Base as B
 from models.rectangle import Rectangle as R
 
 class TestRectangle(unittest.TestCase):
+    """Test cases for a Rectangle class"""
+    def setUp(self) -> None:
+        """Code to run before each test"""
+        B._Base__nb_objects = 0  # reset object counter
+
     def test_new_rectangles_require_both_width_and_height(self):
         height_missing = "TypeError: Rectangle.__init__() missing 1 \
             required positional argument: 'height'"
