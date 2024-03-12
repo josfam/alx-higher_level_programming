2486d5be2679738bbb3889df0b42488d4f823a76
josfam <josephamukun@tutanota.com>
2024-03-13 00:18:41 +0300

    Implement `4. First table`

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- creates a table called first_table in the current database with the details:
+-- id INT
+-- name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
