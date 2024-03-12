f7f808e10ab3b6d1313a9573b363d641530366db
josfam <josephamukun@tutanota.com>
2024-03-12 17:39:57 +0300

    Fix: Re-roder variables

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     for ch in str:
+        ord_ch = ord(ch)
         # shift by 32 to print the upper-cases
-        if (97 <= ord(ch) <= 122):
-            print('{}'.format(chr(ord(ch) - 32)), end='')
+        if (97 <= ord_ch <= 122):
+            print('{}'.format(chr(ord_ch - 32)), end='')
         else:
-            print('{}'.format(ch), end='')
-    print()
+            print(ch, end='')
+
