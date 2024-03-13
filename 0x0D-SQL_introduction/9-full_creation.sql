36c6c8368cadf2b867a2846ec2786f2a77d0786d
josfam <josephamukun@tutanota.com>
2024-03-13 08:58:14 +0300

    Fix tabel name to add the correct table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -9,7 +9,7 @@
 -- id = 2, name = “Alex”, score = 3
 -- id = 3, name = “Bob”, score = 14
 -- id = 4, name = “George”, score = 8
-CREATE TABLE IF NOT EXISTS third_table(
+CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR (256),
 	score INT
