72e83bbc87d507d865850a51959b4714e495398f
josfam <josephamukun@tutanota.com>
2024-03-13 09:05:21 +0300

    Fix: Insert into second_table not third...

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- Creates a table second_table in the database hbtn_0c_0 in the MySQL server
+-- Creates a table second_table in the database hbtn_0c_0 in your MySQL server
 -- and add multiples rows.
 -- second_table description:
 -- id INT
@@ -9,14 +9,14 @@
 -- id = 2, name = “Alex”, score = 3
 -- id = 3, name = “Bob”, score = 14
 -- id = 4, name = “George”, score = 8
-CREATE TABLE IF NOT EXISTS second_table(
+CREATE TABLE IF NOT EXISTS third_table(
 	id INT,
 	name VARCHAR (256),
 	score INT
 );
 
 -- Insert values into table
-INSERT INTO third_table (id, name, score) VALUES (1, "John", 10);
-INSERT INTO third_table (id, name, score) VALUES (2, "Alex", 3);
-INSERT INTO third_table (id, name, score) VALUES (3, "Bob", 14);
-INSERT INTO third_table (id, name, score) VALUES (4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
