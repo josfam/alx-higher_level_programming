5971b5a413dfd9edfb728e4806c960800f950358
josfam <josephamukun@tutanota.com>
2024-03-13 09:08:17 +0300

    Fix: Add multiple values in one go

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -16,7 +16,8 @@ CREATE TABLE IF NOT EXISTS third_table(
 );
 
 -- Insert values into table
-INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
-INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
-INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
-INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
