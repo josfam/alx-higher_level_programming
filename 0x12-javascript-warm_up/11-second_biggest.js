258a6b61d92327aff7550d6a73fc6d0a6bcb8516
josfam <josephamukun@tutanota.com>
2024-04-09 05:02:03 +0300

    Fix: Turn strings into ints before sorting

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,6 +6,8 @@ const numsToSort = process.argv.slice(2);
 if (argLen <= 1) {
   console.log(0);
 } else {
-  numsToSort.sort();
-  console.log(numsToSort[numsToSort.length - 2]);
+  // make strings into ints and sort them
+  const numsAsInts = numsToSort.map((x) => parseInt(x));
+  numsAsInts.sort();
+  console.log(numsAsInts[numsAsInts.length - 2]);
 }
