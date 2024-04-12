66bb03c207499dd2f543cd3b814eeabbafc17a6d
josfam <josephamukun@tutanota.com>
2024-04-12 16:13:09 +0000

    Correct docs for accuracy

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
 """Uses MySQLdb to interact with a MYSQL database.
-Specifically, lists all states with a name starting with `N` (upper N) from the
-database `hbtn_0e_0_usa`
+
+Question:
+Write a script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
 """
 
 if __name__ == '__main__':
