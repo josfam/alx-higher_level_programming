200fb0e5e7da6dda8b6302393ee5e584bf1aecd5
josfam <josephamukun@tutanota.com>
2024-04-12 13:03:58 +0000

    Implement `0. Get all states`

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(
+        user=db_user,
+        password=db_password,
+        database=db_name,
+        host='localhost',
+        port=3306,
+    )
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states ORDER BY id ASC""")
+
+    for pair in cur.fetchall():
+        print(pair)
+
+    cur.close()
