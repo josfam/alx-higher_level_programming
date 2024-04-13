f65f5a0bb92bb4b66dc80cdbf52e4510e72533be
josfam <josephamukun@tutanota.com>
2024-04-13 14:53:20 +0000

    Fix: Don't run the code when imported

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,28 +6,29 @@ Qn:
 Write a script that lists all State objects from the database hbtn_0e_6_usa
 """
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-import sys
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    import sys
 
-db_user = sys.argv[1]
-db_password = sys.argv[2]
-db_name = sys.argv[3]
+    db_user = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
 
-# engine
-engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        db_user, db_password, db_name
+    # engine
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            db_user, db_password, db_name
+        )
     )
-)
 
-# session for db communication
-Session = sessionmaker(bind=engine)
-session = Session()
+    # session for db communication
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-all_states = session.query(State).order_by(State.id.asc())
-for state in all_states:
-    print('{}: {}'.format(state.id, state.name))
+    all_states = session.query(State).order_by(State.id.asc())
+    for state in all_states:
+        print('{}: {}'.format(state.id, state.name))
 
-session.close()
+    session.close()
