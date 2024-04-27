15004a6f4fec3b2115d39c2a4d8586099153c7de
josfam <jamukun@proton.me>
2024-04-27 16:46:20 +0300

    Implement `0. What's my status? #0`

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__ == '__main__':
+    import urllib.request
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print('Body response:')
+        print(f'\t- type: {type(content)}')
+        print(f'\t- content: {content}')
+        print(f'\t- utf8 content: {content.decode(encoding="utf-8")}')
