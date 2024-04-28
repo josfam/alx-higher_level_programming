8f1e078ffb4b9a09cb1b8a1f1c769f4fe8b98a61
josfam <jamukun@proton.me>
2024-04-28 14:03:07 +0300

    Implement `9. My GitHub!`

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Takes GitHub credentials and uses the GitHub API to display the user's id"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    username = sys.argv[1]
+    access_token = sys.argv[2]
+
+    url = 'https://api.github.com/users/{username}'.format(username)
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': f'token {access_token}',
+    }
+
+    req = requests.request('GET', url, headers=headers)
+    json_response = req.json()
+    if json_response.get('message', None) == 'Bad credentials':
+        print('None')
+    else:
+        print(json_response.get('id'))
