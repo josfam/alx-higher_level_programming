0912344b5dcfd93894dcd4abc14a61f0ff8d5f7c
josfam <jamukun@proton.me>
2024-04-28 14:02:17 +0300

    Remove unecessary f strings, and fix pycodestyle

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
 
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url).text
-    print(f'Body response:')
+    print('Body response:')
     print(f'\t- type: {type(response)}')
     print(f'\t- content: {response}')
